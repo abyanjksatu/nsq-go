d2fa1f962bbf327c8b02131e0426d8bd480a290c
kecci <kecci@machtwatch.co.id>
2021-02-04 10:10:17 +0700

    add 2 method

--- segmentio/nsq.go
+++ segmentio/nsq.go
@@ -8,55 +8,35 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-func main() {
-	ctx := context.Background()
-	g, _ := errgroup.WithContext(ctx)
-
-	// Producer
-	g.Go(func() error {
-		return Producer()
-	})
-	if err := g.Wait(); err != nil {
-		fmt.Println(err.Error())
+type (
+	HelloNsq interface {
+		Publisher() *nsq.Producer
+		Consumer() *nsq.Consumer
+		Publish([]byte) error
+		Consume(func(msg *nsq.Message) error) error
 	}
 
-	// Consumer
-	g.Go(func() error {
-		return Consumer()
-	})
-	if err := g.Wait(); err != nil {
-		fmt.Println(err.Error())
+	HelloNsqImpl struct {
+		NsqConsumer *nsq.Consumer
+		NsqProducer *nsq.Producer
 	}
-}
+)
 
-func Producer() error {
+func NewHelloNsq() HelloNsq {
 	// Starts a new producer that publishes to the TCP endpoint of a nsqd node.
 	// The producer automatically handles connections in the background.
-	producer, _ := nsq.StartProducer(nsq.ProducerConfig{
+	producer, err := nsq.NewProducer(nsq.ProducerConfig{
 		Topic:   "hello",
 		Address: "localhost:4150",
 	})
-
-	// Publishes a message to the topic that this producer is configured for,
-	// the method returns when the operation completes, potentially returning an
-	// error if something went wrong.
-	err := producer.Publish([]byte("Hello World!"))
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
-	// Stops the producer, all in-flight requests will be canceled and no more
-	// messages can be published through this producer.
-	producer.Stop()
-
-	return nil
-}
-
-func Consumer() error {
 	// Create a new consumer, looking up nsqd nodes from the listed nsqlookup
 	// addresses, pulling messages from the 'world' channel of the 'hello' topic
 	// with a maximum of 250 in-flight messages.
-	consumer, err := nsq.StartConsumer(nsq.ConsumerConfig{
+	consumer, err := nsq.NewConsumer(nsq.ConsumerConfig{
 		Topic:       "hello",
 		Channel:     "world",
 		Address:     "localhost:4150",
@@ -66,15 +46,116 @@ func Consumer() error {
 		fmt.Println(err.Error())
 	}
 
+	return &HelloNsqImpl{
+		NsqProducer: producer,
+		NsqConsumer: consumer,
+	}
+}
+
+func (h *HelloNsqImpl) Publisher() *nsq.Producer {
+	return h.NsqProducer
+}
+
+func (h *HelloNsqImpl) Consumer() *nsq.Consumer {
+	return h.NsqConsumer
+}
+
+func (h *HelloNsqImpl) Publish(message []byte) error {
+	h.NsqProducer.Start()
+	// Publishes a message to the topic that this producer is configured for,
+	// the method returns when the operation completes, potentially returning an
+	// error if something went wrong.
+	err := h.NsqProducer.Publish(message)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// Stops the producer, all in-flight requests will be canceled and no more
+	// messages can be published through this producer.
+	h.NsqProducer.Stop()
+	return nil
+}
+
+func (h *HelloNsqImpl) Consume(handler func(msg *nsq.Message) error) error {
+	h.NsqConsumer.Start()
 	// Consume messages, the consumer automatically connects to the nsqd nodes
 	// it discovers and handles reconnections if something goes wrong.
-	for msg := range consumer.Messages() {
-		fmt.Println(string(msg.Body))
+	for msg := range h.NsqConsumer.Messages() {
+		err := handler(&msg)
+		if err != nil {
+			return err
+		}
 		msg.Finish()
-		if len(consumer.Messages()) == 0 {
+		if len(h.NsqConsumer.Messages()) == 0 {
 			break
 		}
 	}
-	consumer.Stop()
-	return err
+	h.NsqConsumer.Stop()
+	return nil
+}
+
+func (h *HelloNsqImpl) WaitConsume(handle func(msg *nsq.Message) error) error {
+	h.NsqConsumer.Start()
+	// Consume messages, the consumer automatically connects to the nsqd nodes
+	// it discovers and handles reconnections if something goes wrong.
+	for msg := range h.NsqConsumer.Messages() {
+		err := handle(&msg)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		msg.Finish()
+	}
+	return nil
+}
+
+func main() {
+	helloNsq := NewHelloNsq()
+	ctx := context.Background()
+	g, _ := errgroup.WithContext(ctx)
+
+	// Producer
+	g.Go(func() error {
+		return helloNsq.Publish([]byte("Hello World!"))
+	})
+	if err := g.Wait(); err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// Consumer
+	g.Go(func() error {
+		return helloNsq.Consume(func(msg *nsq.Message) error {
+			fmt.Println(string(msg.Body))
+			return nil
+		})
+	})
+	if err := g.Wait(); err != nil {
+		fmt.Println(err.Error())
+	}
+
+	// // Producer
+	// g.Go(func() error {
+	// 	helloNsq.Publisher().Start()
+	// 	err := helloNsq.Publisher().Publish([]byte("HALO"))
+	// 	defer helloNsq.Publisher().Stop()
+	// 	return err
+	// })
+	// if err := g.Wait(); err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+
+	// // Consumer
+	// g.Go(func() error {
+	// 	helloNsq.Consumer().Start()
+	// 	for msg := range helloNsq.Consumer().Messages() {
+	// 		fmt.Println(string(msg.Body))
+	// 		if len(helloNsq.Consumer().Messages()) == 0 {
+	// 			break
+	// 		}
+	// 	}
+	// 	helloNsq.Consumer().Stop()
+	// 	return nil
+	// })
+	// if err := g.Wait(); err != nil {
+	// 	fmt.Println(err.Error())
+	// }
 }
